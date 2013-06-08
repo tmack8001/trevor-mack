e307bdfd6977cecc7775f02268db0bef5847a6cd
Trevor Mack <tmack@groupon.com>
2013-06-08 01:57:51 -0400

    renamed onlinecv.php and initially changed a bunch of image extensions from JPG to jpg

--- includes/php/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-//session_start();
-//Define constants here
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'trevormack');
-define('DB_PASSWORD', 'trevorpassword');
-define('DB_NAME', 'trevormack');
-?>
