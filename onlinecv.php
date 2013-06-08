0be0246897cb307eff1716e96f8b65cf1cd71d2e
Trevor Mack <tmack@groupon.com>
2013-06-08 02:00:39 -0400

    renamed onlinecv.php

---
+++ onlinecv.php
@@ -0,0 +1,286 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Trevor Mack | CV | Computer Science Student</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Trevor Mack is a Computer Science Student at RIT and freelance PHP/MySQL web developer on the side focusing on database-driven websites." />
+	<meta name="keywords" content="php developer, mysql developer, java developer, php programmer, php/mysql developer, php/mysql programmer, web developer, freelancer, java programming, java, opensocial programmer, opensocial" />
+		
+	<link href="/includes/css/styles.css" media="all" rel="stylesheet" type="text/css" />
+	<link href="/includes/css/resume.css" media="all" rel="stylesheet" type="text/css" />
+	<link href="/includes/css/print.css" media="print" rel="stylesheet" type="text/css" />
+		
+	<script src="/lib/jquery-1.3.2.js" type="text/javascript"></script>
+	
+	<script src="/lib/cufon/cufon-yui.js" type="text/javascript"></script>
+	<script src="/lib/cufon/Aller.font.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		Cufon.replace('h1');
+		Cufon.replace('.item h2');
+		Cufon.replace('h2.cv-section-name');
+	</script>
+	
+	<link rel="stylesheet" type="text/css" href="/lib/fancybox/jquery.fancybox-1.2.6.css" media="screen" />
+	<script type="text/javascript" src="/lib/fancybox/jquery.fancybox-1.2.6.pack.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$("a.lightbox").fancybox({
+				'overlayOpacity': 0.7,
+				'overlayColor'	: '#333'
+			});
+		});
+	</script>
+
+	<link rel="stylesheet" type="text/css" href="/lib/tipsy/stylesheets/tipsy.css" />
+	<script type="text/javascript" src="/lib/tipsy/javascripts/jquery.tipsy.js"></script>
+	<script type='text/javascript'>
+	  $(function() {
+		$('.tip').tipsy({gravity: 'n'});
+	  });
+	</script>
+		
+</head>
+<body>
+
+<div id="wrapper">
+
+		<div id="side_trevor_mack"></div>
+
+	    <div id="main_holder">
+	    	<div id="header">
+				<div id="nav">
+					<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/layout/top-navigation.php'); ?>
+				</div>
+				<div id="banner"></div>
+			</div>
+			<div id="content_holder">
+				<div id="cv">
+					<div class="print-border first"></div>
+					
+					<div id="person" class="cv-section first-cv-section fixed">
+						<div class="person">
+							<img src="/images/trevormack_100x100.png" width="100" height="100" alt="trevor mack" />
+							<h1>Trevor Mack</h1>
+							<h2>Computer Science Student</h2>
+			
+						</div>
+					
+						<div id="contact-info">
+							<ul id="social-media">
+								<li><a href="http://www.flickr.com/photos/trevormack" title="Flickr Profile" class="tip"><img src="/images/webSocialIcons/flickr.png" alt="flickr icon" /></a></li>
+								<li><a href="http://twitter.com/tmack8001" title="Twitter Profile" class="tip"><img src="/images/webSocialIcons/twitter.png" alt="twitter icon" /></a></li>
+								<li><a href="http://www.facebook.com/trevormack" title="Facebook Profile" class="tip"><img src="/images/webSocialIcons/facebook.png" alt="facebook icon" /></a></li>
+								<li><a href="http://www.linkedin.com/in/trevormack" title="LinkedIn Profile" class="tip"><img src="/images/webSocialIcons/linkedin.png" alt="linkedin icon" /></a></li>
+								<li><a href="http://www.google.com/profiles/Drummer8001" title="Google Profile" class="tip"><img src="/images/webSocialIcons/google.png" alt="contact me" /></a></li>
+								<li><a href="http://picasaweb.google.com/Drummer8001" title="Picasa Profile" class="tip"><img src="/images/webSocialIcons/picasa.png" alt="contact me" /></a></li>
+							</ul>
+							<ul id="contact">
+								<li>email: <a href="mailto:drummer8001@gmail.com">drummer8001@gmail.com</a></li>
+								<li>website: <a href="http://trevor-mack.com/">www.trevor-mack.com</a></li>
+								<li>phone: <strong>+1 (585) 563-9636</strong></li>
+			
+								<li style="margin-top:20px;"><i><u>Last updated:</u></i> <strong>01/28/2010</strong></li>
+							</ul>
+						</div> <!-- end #contact-info -->
+			
+					</div><!-- end .first-cv-section -->
+					<div id="about-me" class="cv-section fixed">
+						<h2 class="cv-section-name">About Me</h2>
+						<div class="cv-section-items">
+							
+							<div class="item last-item">
+								<p>Current Computer Science student at RIT, Rochester Institute of Technology with 2 years of practical, 
+								real industry experience in developing high-quality code for dynamic web applications. Good communication 
+								and presentation skills with a logical approach to problem-solving. Have had experience working in both 
+								large and small group settings. Can and will strive in a non-managed position or working environment, 
+								and is self motivated.</p>
+								<p>Experienced developer in both desktop and web-based systems, with the understand of how to take 
+								a project from initial design to final product through a number of design and development stages. Also have
+								experience with System Administration through setting up a professional server environment personal use.</p>
+								<p>Have interest with advanced algorithms, data representation, and artificial intelligence. While in school
+								make a cluster out of complexity and computability along with AI. Have a nack for trying to stay up today on
+								the lastest and greated gadgets and technologies. Very active in the beta testing community regarding public
+								webservices and applications.</p>
+							</div><!-- end .item -->
+							
+						</div><!-- end .cv-section-items -->
+					</div><!-- end .cv-section -->
+				
+					<div id="work-experience" class="cv-section fixed">
+						<h2 class="cv-section-name">Work Experience</h2>
+						<div class="cv-section-items">
+							
+							<div class="item">
+								<h4>March 2009 - October 2009</h4>
+								<h2>Co-op Developer</h2>
+								<h3>MITRE Corporation, Full-Time</h3>
+								<br />
+								<ul>
+									<li>Evaluated the business advantage of creating academic relationships with universities.</li>
+									<li>Researched and evaluated Google Wave for implementation into an Enterprise environment.</li>
+									<li>Developed multiple applications that adhered to the OpenSocial Gadget API these included: finding 
+									currently available conference rooms, airline and aiport statuses, and idea posting/ranking gadgets.</li>
+								</ul>
+							</div><!-- end .item -->
+									
+							<div class="item last-item">
+								<h4>March 2008 - September 2008</h4>
+								<h2>Co-op Software Developer</h2>
+								<h3>Bytronics Incorporated, Full-Time</h3>
+								<br />
+								<ul>
+									<li>Directly communicated with utility companies to customize their experience with the application. 
+									These requests ranged from bug fixes to customer enhancements and new modules.</li>
+									<li>Worked alongside a team of 5 developers to enhance and implement new features to a web-based 
+									utility ticket management application which is used nationwide.</li>
+								</ul>
+							</div><!-- end .item -->
+							
+						</div><!-- end .cv-section-items -->
+					</div><!-- end .cv-section -->
+					
+					<div id="education" class="cv-section fixed">				
+						<h2 class="cv-section-name">Education</h2>
+						<div class="cv-section-items">
+							
+							<div class="item last-item">
+								<h4>September 2006 to Present</h4>
+								<h2>Rochester Institute of Technology</h2>
+								<h3>Computer Science and Economics Double Major</h3>
+								<br />
+								<p>Currently a fifth year student enrolled as a dual Computer Science and Economics major pending 
+								graduation in August 2010. Have made several programs in Java and C++ which involved the use of GUIs, 
+								advanced algorithms, and/or network communication. I have learned how to implement the Software Development 
+								Lifecycle and how a professional development team is comprised and corroborates with each member during this 
+								cycle.</p>
+								<ul>
+									<li><b>GPA: </b> (overall): 3.173 / 4.0</li>
+									<li><b>GPA: </b> (PFOS): 3.250 / 4.0</li>
+								</ul>
+							</div><!-- end .item -->
+							
+						</div><!-- end .cv-section-items -->
+					</div><!-- end .cv-section -->
+					
+					<div class="pagebreak"></div>
+					<div class="print-border"></div>
+					
+					<div id="technical-skills" class="cv-section fixed">
+						<h2 class="cv-section-name">Technical Skills</h2>
+						<div class="cv-section-items">
+							
+							<div class="item">
+								<h4>Intermediate, 1 year</h4>
+								<h2>PHP &amp; MySQL Development</h2>
+								<br />
+								<p>Having only started web-based development 2 years ago, I still have much to learn about PHP 4 &amp; 5, but I do have an intermediate understanding
+								of PHP (both Object-oriented and Procedural). Most of my knowledge in PHP has come from independant studies and online tutorials over the past couple of years.</p>
+								<ul>
+									<li>Designed SQL/PHP Rest services for use in websites and applications</li>
+									<li>Developed an access control list (ACL) with user authorization using encrypted sessions and cookies</li>
+									<li>Created a custom content management system (CMS)</li>
+									<li>Used the <a href="http://phpmailer.sourceforge.net">phpMailer</a> object to send emails server-side</li>
+									<li>Knowledge of <a href="http://www.zend.com/en/">Zend Studio</a>, <a href="http://eclipse.org/">Eclipse</a>, and <a href="http://www.aptana.org/">Aptana</a>.</li>
+								</ul>
+							</div><!-- end .item -->
+							
+							<div class="item">
+								<h4>Intermediate, 2 years</h4>
+								<h2>Javascript Programming</h2>
+								<br />
+								<p>I began using Javascript for dynamic elements in websites using AJAX (XmlHTTPRequest), to incorporate with other PHP/MySQL applications and services.</p>
+								<ul>
+									<li>Script creation from scratch, or using the <a href="http://jquery.com/">jQuery</a> framework along with the <a href="http://www.opensocial.org/">OpenSocial</a> APIs</li>
+									<li>Integration with third-party web services (XML and JSON) to product AJAX rich application components (including <a href="http://www.flightstats.com">FlightStats</a>, <a href="http://twitter.com">twitter</a>, <a href="http://friendfeed.com">friendfeed</a>, blogs, etc.)</li>
+									<li>Live in-browser validation and submission of user input using custom scripts and <a href="http://livevalidation.com/">live validation</a></li>
+									<li>Visual effects, components, and animations using <a href="http://jqueryui.com/">jQueryUI</a> and <a href="http://developer.yahoo.com/yui/">YUI</a> User Interface frameworks</li>
+								</ul>
+							</div><!-- end .item -->
+									
+							<div class="item">
+								<h4>Beginner, 1 year</h4>
+								<h2>Server Administration</h2>
+								<br />
+								<p>I have experience with setting up, administrating and maintaining both Windows (<a href="http://www.microsoft.com/windowsserver2003/default.mspx">Server 2003</a> &amp; <a href="http://www.microsoft.com/windowsserver2008/en/us/default.aspx">2008</a>) 
+								and Linux (<a href="http://www.ubuntu.com/">Ubuntu</a> &amp; <a href="http://fedoraproject.org/">Fedora</a>) servers. Ability to configure a webserver to use HTTP-Secure (HTML with SSL/TLS) 
+								with a self-signed certificate, PHP, and MySQL.</p>
+								<ul>
+									<li>Mail server management including spam prevention</li>
+									<li>DNS zone administration using an online DNS server, <a href="http://www.everydns.com/">EveryDNS</a></li>
+									<li>Web (<a href="http://www.apache.org/">Apache</a>, <a href="http://www.wampserver.com/en/">WAMP Server</a>) &amp; Database (<a href="http://www.mysql.com/">MySQL</a>, <a href="http://www.microsoft.com/sqlserver/2008/en/us/default.aspx">Microsoft SQL 2008</a>) server operation, configuration, and maintenance</li>
+									<li>Version control using Subversion and CVS systems</li>
+								</ul>
+							</div><!-- end .item -->
+						
+							<div class="item last-item">
+								<h4>Beginner, 1 year</h4>
+								<h2>C++ Programming</h2>
+								<br />
+								<p>While studing for my degree in Computer Science we covered basic console based C++ application development, 
+								using both Windows and Linux-based compilers.</p>	
+							</div><!-- end .item -->
+							
+						</div><!-- end .cv-section-items -->
+					</div><!-- end .cv-section -->
+					
+					<div class="pagebreak"></div>
+					<div class="print-border"></div>
+					
+					<div id="interests" class="cv-section fixed">
+						<h2 class="cv-section-name">Interests</h2>
+						<div class="cv-section-items">
+						
+							<div class="item">
+								<h2>Hockey</h2>
+								<br />
+								<p>Hockey has always been something that has sparked my interest, lately (since 2008) I have gotten
+								myself into playing hockey. Now I am hooked, I play at least once a week and watch every one of our 
+								school's home hockey games. Go Tigers!</p>
+							</div><!-- end .item -->
+							
+							<div class="item last-item">
+								<h2>Lastest Gadgets and Technology</h2>
+								<br />
+								<p>I have always been intrigued by the latest and greatest gadgets, applications, and webservices. 
+								I like to know what is out there on the market and look at the pros and cons of new devices and 
+								services. I know this is very common with technology students and professionals, but I have begun 
+								blogging (not always seriously) about some of these gadgets when they come to market.</p>
+							</div><!-- end .item -->
+						</div><!-- end .cv-section-items -->
+					</div><!-- end .cv-section -->
+		
+					<div id="references" class="cv-section last-cv-section fixed">
+						<h2 class="cv-section-name">References</h2>
+						<div class="cv-section-items">
+							
+							<div class="item last-item">
+								<h2>Email for Details</h2>
+								<br />
+								<p>For privacy reasons my references are excluded from this page. Please email me for these details if they are required.</p>	
+							</div><!-- end .item -->
+							
+						</div><!-- end .cv-section-items -->
+					</div><!-- end .cv-section -->
+					
+				</div><!-- end #cv -->			
+			</div> <!--end #content_holder -->
+
+		    <div id="footer">
+				<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/layout/footer.php'); ?>
+			</div> <!--  end #footer -->
+			
+		</div> <!-- end #main_holder -->
+	</div> <!-- end #wrapper -->
+
+	<!--GoogleAnalytics-->
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	var pageTracker = _gat._getTracker("UA-5228215-1");
+	pageTracker._trackPageview();
+	</script>
+</body>
+
+</html>
